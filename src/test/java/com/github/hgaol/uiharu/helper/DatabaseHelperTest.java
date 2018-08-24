53f257c369dc5d7934edff857cee5237139242ca
hgaol <dhangao@hotmail.com>
2018-08-24 23:09:44 +0800

    :zap:finish database helper

---
+++ src/test/java/com/github/hgaol/uiharu/helper/DatabaseHelperTest.java
@@ -0,0 +1,18 @@
+package com.github.hgaol.uiharu.helper;
+
+import com.github.hgaol.uiharu.demo.Customer;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseHelperTest {
+
+//    @Test
+//    public void queryEntity() {
+//        System.out.println(DatabaseHelper.queryEntity(Customer.class, "select * from customer where id = ?", "1"));
+//    }
+//
+//    @Test
+//    public void queryEntityList() {
+//    }
+}
